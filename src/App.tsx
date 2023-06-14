5c003deea4ba5f6663cbd341429f805a630ed649
Marat252552 <117304418+Marat252552@users.noreply.github.com>
2023-06-14 21:26:05 +0300

    0.5

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Navigate, Route, Routes } from 'react-router-dom'
 import LoginPage from './components/pages/LoginPage'
 import MainPage from './components/pages/MainPage'
 import { Provider } from 'react-redux'
@@ -10,14 +10,14 @@ const App = () => {
   return (
     <div style={{ width: '100%' }}>
       <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path='/login' Component={LoginPage} />
             <Route path='/main' Component={MainPage} />
             <Route path='*' element={<Navigate to='/login' />} />
           </Routes>
 
-        </BrowserRouter>
+        </HashRouter>
       </Provider>
 
 
